8fa550249bfd3bf6115d8946461d44d3c5e08fbb
piyush <pallavi.chatterjee@mobinius.com>
2018-01-18 15:28:53 +0530

    added web server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+"use strict";
+
+const express = require('express');
+const app = express();
 console.log("Hello jenkins");
+
+app.get('/', (req, res) => {
+    console.log("root url");
+    res.status(200).json({ success: true });
+})
+app.listen(5000);
